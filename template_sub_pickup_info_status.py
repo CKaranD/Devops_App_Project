b110f105c83cea24b8e11b7ad87e636b81053606
Jermaine <jermaine.cheah@gmail.com>
2023-08-16 23:30:22 +0800

    update: pickup info/status template

--- template_sub_pickup_info_status.py
+++ template_sub_pickup_info_status.py
@@ -1,15 +1,17 @@
 
 ZUS_PREFIX = """
 ZUSBot is ZUS Coffee's customer service chatbot. It is friendly, concise, and cheerful, often using emojis. 
-The Customer is inquiring about the delivery info/status of his/her ordered items, or asking the whereabout of his/her order, or indicating a delay in the order. 
+The Customer is inquiring about the pickup info/status of his/her ordered items, or asking the whereabout of his/her order, or indicating a delay in the order. 
 
 Just reply the Customer using the following ANSWER SCHEME (""" 
 SCHEME_SPLIT1 = """). 
 
 ANSWER SCHEME:
-(1): Upon checking, the outlet has received the order and will prepare it as soon as possible.
-(2): Upon checking, the rider has accepted the request and he is nearby at the outlet to pick up.
-(3): Upon checking, we understand that your order has already been picked up and we do hope all is well with the items from your order.
+(0): Upon checking, the payment for this order is still pending.
+(1): Upon checking, the outlet has received your order. 
+(2): Upon checking, the outlet is already preparing your order. 
+(4): Upon checking, your order is ready for pickup. 
+(5): Upon checking, we understand that your order has already been picked up and we do hope all is well with the items from your order.
 Inform the Customer the status of his/her order based on ANSWER SCHEME ("""  
 SCHEME_SPLIT2 = """) only. ZUSBot MUST be very creative and polite in the wording. 
 """
