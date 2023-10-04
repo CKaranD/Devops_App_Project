1977959578ae057dfc37c4b187aa509878704154
Jermaine <jermaine.cheah@gmail.com>
2023-10-04 23:56:53 +0800

    update: vecdb for all 3, tested with different chunk size as well

--- load_annoydb.py
+++ load_annoydb.py
@@ -11,17 +11,17 @@ openai_api_key = str(openai_key[0])
 
 # loader = DirectoryLoader(os.environ['LOAD_DIR'])
 
-loader = TextLoader('db_text/outlet_details_clean.txt')
+# loader = TextLoader('db_text/product_menu_clean.txt')
 # loader = TextLoader('db_text/outlet_details_clean.txt')
-# loader = TextLoader('db_text/combined_loyalty_benefit_birthday_voucher.txt')
+loader = TextLoader('db_text/combined_loyalty_benefit_birthday_voucher.txt')
 
 documents = loader.load()
 
-# product_menu_clean: chunk size 1000, overlap 0
+# product_menu_clean: chunk size 1650, overlap 0
 # outlet chunk size = 500
 # loyalty/birthday chunk size = 1600
 
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+text_splitter = CharacterTextSplitter(chunk_size=1600, chunk_overlap=0)
 
 docs = text_splitter.split_documents(documents)
 vectordb = Annoy.from_documents(
@@ -29,7 +29,7 @@ vectordb = Annoy.from_documents(
     embedding=OpenAIEmbeddings(openai_api_key=openai_api_key))
 
 # vectordb.save_local("db/products_menu")
-vectordb.save_local("db/outlet_details")
+# vectordb.save_local("db/outlet_details")
 
-# vectordb.save_local("db/birthday_vouchers")
-# vectordb.save_local("db/loyalty_benefits")
+vectordb.save_local("db/birthday_vouchers")
+vectordb.save_local("db/loyalty_benefits")
