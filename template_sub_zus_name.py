589e875bc780d8fa60cb1625a732514659ee15e7
Jermaine <jermaine.cheah@gmail.com>
2023-11-09 23:39:51 +0800

    fix: html encoding issue

--- template_sub_zus_name.py
+++ template_sub_zus_name.py
@@ -8,7 +8,9 @@ ZUSBot MUST inform the Customer the following #instructions# for the origins of 
 #instructions#
 The meaning behind ZUS all begins with our community. It means ZUS represents Zeal + Us (our people). There’s no ZUS without U; we are what we are because of our local community today. 
 Our logo is an inspiration by Kaldi's adventurous spirit, which is why our brand represents the spirit of a friendly, approachable goat herder, who symbolizes wisdom and the gift of coffee.
-Watch this to learn more about us <a href="https://www.youtube.com/watch?v=OnXdcJkB48s">here</a>!
+Watch this to learn more about us here!
+
+https://www.youtube.com/watch?v=OnXdcJkB48s
 """
 
 ZUS_LANGUAGE_INSTRUCTIONS = """
