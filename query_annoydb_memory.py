958b1ce25b4a78884dcc38c6b887d4c10421a953
Jermaine <jermaine.cheah@gmail.com>
2023-12-30 17:32:51 +0800

    refactor: VecDB memory

--- query_annoydb_memory.py
+++ query_annoydb_memory.py
@@ -4,20 +4,21 @@ from langchain.vectorstores import Annoy
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 from langchain.memory import (CombinedMemory, ConversationBufferMemory,
-                            ConversationSummaryMemory) 
+                            ConversationSummaryMemory, ConversationBufferWindowMemory) 
 
 from get_openai_key import openai_api_key
 
 def create_query_memory():
     llm=ChatOpenAI(openai_api_key=openai_api_key, 
-            model_name='gpt-3.5-turbo',
+            model_name='gpt-3.5-turbo-1106',
             temperature=0.6,
             max_tokens = 2048)
     
-    conv_memory = ConversationBufferMemory(
+    conv_memory = ConversationBufferWindowMemory(
+        k=3,
         memory_key="chat_history_lines",
         input_key="question", 
-        return_messages=True,
+        return_messages=False,
         human_prefix="Customer",
         ai_prefix="ZUSBot")
 
@@ -26,7 +27,6 @@ def create_query_memory():
                                                human_prefix="Customer",
                                                ai_prefix="ZUSBot")
     memory = CombinedMemory(memories=[conv_memory, summary_memory])
-    # memory = ConversationBufferMemory(memory_key="history", input_key="question")
     return memory
 
 
@@ -37,18 +37,19 @@ def get_qa_chain(db_dir, memory, vecdb_prompt_template):
     )
 
     chain_type_kwargs = {"prompt": PROMPT,
-                        "memory": memory}
+                        "memory": memory,
+                        "verbose": False}
 
     vectordb = Annoy.load_local(db_dir, embeddings=OpenAIEmbeddings(openai_api_key=openai_api_key))    
     retriever = vectordb.as_retriever(search_kwargs={"k": 1}) # define how many documents in the dir
 
     ans = RetrievalQA.from_chain_type(
             llm=ChatOpenAI(openai_api_key=openai_api_key, 
-                        model_name='gpt-3.5-turbo',
+                        model_name='gpt-3.5-turbo-1106',
                         temperature=0.3,
                         max_tokens=1650), 
             chain_type="stuff", 
-            retriever=retriever,
+            retriever=retriever,            
             chain_type_kwargs=chain_type_kwargs)
 
     return ans, memory
